158b5841e0a58b6558029f60fc8f0e1e698d9d34
Anđela Popović <andjela9popovic@gmail.com>
2022-05-28 11:18:53 +0200

    Add project files.

---
+++ Prodavnica/Artikal.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prodavnica
+{
+    class Artikal
+    {
+        private double cena;
+        private int barKod;
+        private double porez;
+        private double pdv;
+        private string poreklo;
+        private bool vegan;
+        private bool maloletni;
+        private double carina;
+        private bool osnovnaNamirnica;
+
+        #region Geteri
+
+        public double getCena()
+        {
+            return cena;
+        }
+        public int getBarKod()
+        {
+            return barKod;
+        }
+        public double getPorez()
+        {
+            return porez;
+        }
+        public double getPdv()
+        {
+            return pdv;
+        }
+        public string getPoreklo()
+        {
+            return poreklo;
+        }
+        public bool getVegan()
+        {
+            return vegan;
+        }
+        public bool getMaloletni()
+        {
+            return maloletni;
+        }
+        public double getCarina()
+        {
+            return carina;
+        }
+        public bool getOsnovnaNamirnica()
+        {
+            return osnovnaNamirnica;
+        }
+
+
+        #endregion 
+
+    }
+}
---
+++ Prodavnica/Sektor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prodavnica
+{
+    class Sektor
+    {
+        private string naziv;
+        private int brojZaposlenih;
+        private double povrsina;
+        private int maxBrojArtikala;
+        private int sifraSektora;
+        private double plata;
+
+        #region Geteri
+        public string getNaziv()
+        {
+            return naziv;
+        }
+        public int getBarKod()
+        {
+            return barkod;
+        }
+        public int getbrojZaposlenih()
+        {
+            return brojZaposlenih;
+        }
+        public double getPovrsina()
+        {
+            return povrsina;
+        }
+        public int getMaxBrojArtikala()
+        {
+            return maxBrojArtikala;
+        }
+        public int getSifraSektora()
+        {
+            return sifraSektora;
+        }
+        public double getPlata()
+        {
+            return plata;
+        }
+        #endregion
+
+
+
+
+
+
+    }
+}
